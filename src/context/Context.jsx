b3995d41d8af01577325122e50929ea25a11cceb
ahmed alhalfa <ah.alhalfa@gmail.com>
2024-10-03 22:56:14 +0300

    desktop view is done

---
+++ src/context/Context.jsx
@@ -0,0 +1,72 @@
+import { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import runChat from '../config/gemini';
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState('');
+  const [recentPrompt, setRecentPrompt] = useState('');
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState('');
+
+  const delayParam = (index, nextWord) => {
+    setTimeout(() => {
+      setResultData((prev) => prev + nextWord);
+    }, index * 35);
+  };
+
+  const newChat = () => {
+    setLoading(false);
+    setShowResult(false);
+  }
+
+  const onSent = async (prompt) => {
+    setResultData('');
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(prompt || input);
+    if (!prompt) setPrevPrompts([...prevPrompts, input]);
+    const result = await runChat(prompt || input);
+    let resultArray = result.split('**');
+    let resultBold = '';
+    for (let i = 0; i < resultArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        resultBold += resultArray[i];
+      } else {
+        resultBold += '<b>' + resultArray[i] + '</b>';
+      }
+    }
+    let resultLinesSplit = resultBold.split('*').join('<br>');
+    let resultDelayed = resultLinesSplit.split(' ');
+    for (let i = 0; i < resultDelayed.length; i++) {
+      delayParam(i, resultDelayed[i] + ' ');
+    }
+    setLoading(false);
+    setInput('');
+  };
+
+  const contextValue = {
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+    newChat,
+  };
+
+  return <Context.Provider value={contextValue}>{props.children}</Context.Provider>;
+};
+
+ContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export { ContextProvider };
